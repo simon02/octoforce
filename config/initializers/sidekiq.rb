7da8b5cf104f3c34a2b90e52d0cd5b2b73eb5539
Simon Buelens <simon02@Simons-MacBook-Air.local>
2016-02-10 17:38:57 +0100

    setting up sidekiq w redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,13 @@ schedule_file = "config/schedule.yml"
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+# three unicorns = 3 connections
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+# so one sidekiq can have 5 connections
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 5 }
+end
