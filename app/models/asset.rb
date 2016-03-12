257a8f4f11d13dfba16cc549aebf669fe772d41c
Simon Buelens <simon02@Simons-MacBook-Air.local>
2016-03-12 22:23:38 +0100

    pls work paperclip

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,6 +2,8 @@ class Asset < ActiveRecord::Base
   belongs_to :user
   has_many :posts
   has_many :updates
-  has_attached_file :media, styles: { thumb: "200x200>" }
+  has_attached_file :media, styles: { thumb: "200x200>" },
+            :storage => :s3,
+            :bucket  => ENV['S3_BUCKET_NAME']
   validates_attachment :media, content_type: { content_type: /\Aimage\/.*\Z/ }, size: { less_than: 2.megabytes }
 end
