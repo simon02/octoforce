ca78187abc1883c88adcc052d95620ab21e695d5
Simon Buelens <simon02@Simons-MacBook-Air.local>
2016-04-02 16:20:04 +0200

    refactoring

--- app/workers/queue_worker.rb
+++ app/workers/queue_worker.rb
@@ -1,15 +1,19 @@
 class QueueWorker
   include Sidekiq::Worker
 
-  # def perform schedule_id
-  #   schedule = Schedule.find(schedule_id)
-  #   schedule.reschedule(2) if schedule.outdated? && schedule.identity
-  # end
+  def perform list_id, start_time = nil, end_time = nil
+    start_time = Time.at start_time if start_time.is_a? Integer
+    end_time = Time.at end_time if end_time.is_a? Integer
 
-  def perform list_id
     list = List.find_by id: list_id
     return if !list || list.timeslots.empty? || list.posts.empty?
-    list.reschedule(2) if list
+    if start_time.nil? && end_time.nil?
+      list.reschedule(2)
+    elsif end_time.nil?
+      list.schedule_between start_time, Time.now + 2.weeks
+    else
+      list.schedule_between start_time, end_time
+    end
   end
 
 end
--- app/workers/url_worker.rb
+++ app/workers/url_worker.rb
@@ -4,10 +4,10 @@ class UrlWorker
   def perform update_id
     update = Update.find update_id
     bitly = update.user.bitly_client
-    update.update(text: replace_with_bitly_links(update.text, bitly)) if bitly
+    update.update(text: self.replace_with_bitly_links(update.text, bitly)) if bitly
   end
 
-  def replace_with_bitly_links text, bitly_client
+  def self.replace_with_bitly_links text, bitly_client
     links = URI.extract text
     links.map! do |link|
       [link, bitly_client.shorten(link)]
