c1cbf9d0752a342add9abb992fbca50bf9edb42a
Simon Buelens <simon02@Simons-MacBook-Air.local>
2016-04-05 15:15:49 +0200

    working with modals

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,25 +5,35 @@ class FeedsController < ApplicationController
     @categories = current_user.categories
   end
 
-  def new
-    @feed = Feed.new
-  end
-
   def create
     @feed = Feed.new(feed_params)
 
     if @feed.save
       body, ok = SuperfeedrEngine::Engine.subscribe(@feed, {:retrieve => false})
       if !ok
-        redirect_to feeds_url, notice: "Feed was successfully created but we could not subscribe: #{body}"
+        redirect_to feeds_url, alert: "Feed was successfully created but we could not subscribe: #{body}"
       else
         @feed.update active: true
         FeedRetrieverWorker.perform_async @feed.id, params[:retrieve_content]
         redirect_to feeds_url, notice: 'Feed has been added! Currently retrieving feed title.'
       end
     else
-      render :new
+      redirect_to feeds_url, alert: 'Feed could not be created!'
     end
+  end
+
+  def update
+    feed = Feed.find params[:id]
+    feed.update feed_params_edit
+    if !feed.active
+      body, ok = SuperfeedrEngine::Engine.subscribe(@feed, {:retrieve => false})
+      if !ok
+        redirect_to feeds_url, alert: "Feed updated, but could not be activated."
+      else
+        feed.update active: true
+      end
+    end
+    redirect_to feeds_url, notice: "Feed has been updated."
   end
 
   def destroy
@@ -43,7 +53,12 @@ class FeedsController < ApplicationController
 
   # Only allow a trusted parameter "white category" through.
   def feed_params
-    params.require(:feed).permit(:title, :url, :user_id, :category_id)
+    params.require(:feed).permit(:title, :url, :category_id)
+    params[:user_id] = current_user.id
+  end
+
+  def feed_params_edit
+    params.require(:feed).permit(:title, :category_id)
   end
 
 end
