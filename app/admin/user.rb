060e673e0e16b17f626beb01b3bae9af4c7ef7db
Simon Buelens <simon02@Simons-MacBook-Air.local>
2016-04-19 11:58:18 +0200

    playing around

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,1 +1,16 @@
-ActiveAdmin.register User
+ActiveAdmin.register User do
+
+  member_action :impersonate do
+    impersonate_user(resource)
+    redirect_to root_path
+  end
+
+  collection_action :stop_impersonating do
+    stop_impersonating_user
+  end
+
+  action_item :impersonate do
+    link_to 'Impersonate', impersonate_admin_user_path(user)
+  end
+
+end
