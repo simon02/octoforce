1e4e923c739383e6b62d6d5d9b7c3c65db06fdee
Simon Buelens <simon02@Simons-MacBook-Air.local>
2016-04-30 20:48:24 +0200

    safeties to not throttle twitter

--- app/workers/social_media_worker.rb
+++ app/workers/social_media_worker.rb
@@ -1,6 +1,10 @@
 class SocialMediaWorker
   include Sidekiq::Worker
   sidekiq_options unique: :until_executed
+  sidekiq_options :retry => 3
+  sidekiq_retry_in do |count|
+    (retry_count ** 5) + 60 + (rand(30) * (retry_count + 1))
+  end
 
   def perform update_id
     update = Update.find_by id: update_id
